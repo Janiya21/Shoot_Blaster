c1336848ee00061b3ab4577cc1a52a97ffb8764b
JanithSandaru <janithsandaru999@gmail.com>
2022-03-29 19:22:06 +0530

    added canvas

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/*
+var countdown = $("#countdown").countdown360({
+    radius      : 110,
+    seconds     : 5,
+    fontColor   :'#FFFFFF',
+    autostart   :false,
+    onComplete  :function () {
+        $("#countdown").css("display","none");
+        $(".main-content").css("filter","none");
+    }
+});
+
+$("#btnStartGame").click(function (){
+
+    alert("Game will Start!! Press OK");
+
+    $(".first-frame").fadeOut(2000);
+
+    countdown.start();
+});*/
